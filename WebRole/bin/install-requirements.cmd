099df21265c2af49f6468624424b29e5237b9944
Michiya Takahashi <michiya.takahashi@gmail.com>
2012-04-05 14:57:42 +0900

    small cleanup

--- WebRole/bin/install-requirements.cmd
+++ WebRole/bin/install-requirements.cmd
@@ -15,7 +15,8 @@ echo ERROR: %SystemDrive%\Zoo is missing.
 exit /b 1
 )
 
-if "%LOCAL_RESOURCE_TMP_DIR%"=="" set LOCAL_RESOURCE_TMP_DIR=%TMP%\
+if "%LOCAL_RESOURCE_TMP_DIR%"=="" set LOCAL_RESOURCE_TMP_DIR=%TMP%
+if "%LOCAL_RESOURCE_TMP_DIR:~-1%"=="\" set LOCAL_RESOURCE_TMP_DIR=%LOCAL_RESOURCE_TMP_DIR:~0,-1%
 echo temporary download directory is set to [%LOCAL_RESOURCE_TMP_DIR%]
 
 %PYTHON_INSTALL_DIR%\python -c "import sys; print sys.version" | find "64 bit"
@@ -28,22 +29,24 @@ REM install package managers
 :INSTALL_SETUPTOOLS
 echo installing setuptools...
 if exist %PYTHON_INSTALL_DIR%\Scripts\easy_install.exe goto INSTALL_PIP
-powershell -c "(new-object System.Net.WebClient).DownloadFile('http://peak.telecommunity.com/dist/ez_setup.py', '%LOCAL_RESOURCE_TMP_DIR%easy_setup.py')"
-%PYTHON_INSTALL_DIR%\python %LOCAL_RESOURCE_TMP_DIR%easy_setup.py
+powershell -c "(new-object System.Net.WebClient).DownloadFile('http://peak.telecommunity.com/dist/ez_setup.py', '%LOCAL_RESOURCE_TMP_DIR%\easy_setup.py')"
+%PYTHON_INSTALL_DIR%\python %LOCAL_RESOURCE_TMP_DIR%\easy_setup.py
 
 :INSTALL_PIP
 echo installing pip...
-if exist %PYTHON_INSTALL_DIR%\Scripts\pip.exe goto INSTALL_SUBVERSION
+if exist %PYTHON_INSTALL_DIR%\Scripts\pip.exe goto INSTALL_SVN
 %PYTHON_INSTALL_DIR%\Scripts\easy_install pip
 
 
 REM install django-pyodbc
-:INSTALL_SUBVERSION
+:INSTALL_SVN
 echo installing subversion...
-if exist %LOCAL_RESOURCE_TMP_DIR%svn-win32\bin\svn.exe goto INSTALL_PYODBC
-powershell -c "(new-object System.Net.WebClient).DownloadFile('http://downloads.sourceforge.net/project/win32svn/1.7.2/svn-win32-1.7.2.zip', '%LOCAL_RESOURCE_TMP_DIR%svn-win32-1.7.2.zip')"
-%SystemDrive%\Zoo\Tools\7za x -o%LOCAL_RESOURCE_TMP_DIR% -y %LOCAL_RESOURCE_TMP_DIR%svn-win32-1.7.2.zip
-ren %LOCAL_RESOURCE_TMP_DIR%svn-win32-1.7.2 svn-win32
+set SVN_VERSION=1.7.4
+set SVN_DIR=svn-win32-%SVN_VERSION%
+set SVN_INSTALLER=%SVN_DIR%.zip
+if exist %LOCAL_RESOURCE_TMP_DIR%\%SVN_DIR%\bin\svn.exe goto INSTALL_PYODBC
+powershell -c "(new-object System.Net.WebClient).DownloadFile('http://downloads.sourceforge.net/project/win32svn/%SVN_VERSION%/%SVN_INSTALLER%', '%LOCAL_RESOURCE_TMP_DIR%\%SVN_INSTALLER%')"
+%SystemDrive%\Zoo\Tools\7za x -o%LOCAL_RESOURCE_TMP_DIR% -y %LOCAL_RESOURCE_TMP_DIR%\%SVN_INSTALLER%
 
 :INSTALL_PYODBC
 echo installing pyodbc...
@@ -54,31 +57,31 @@ if "%PYTHON_X86%"=="0" goto INSTALL_PYODBC_X64
 goto INSTALL_DJANGO_PYODBC
 
 :INSTALL_PYODBC_X64
-powershell -c "(new-object System.Net.WebClient).DownloadFile('http://pyodbc.googlecode.com/files/pyodbc-3.0.2.win-amd64-py2.7.exe', '%LOCAL_RESOURCE_TMP_DIR%pyodbc.exe')"
-%SystemDrive%\Zoo\Tools\7za x -o%LOCAL_RESOURCE_TMP_DIR%pyodbc -y %LOCAL_RESOURCE_TMP_DIR%pyodbc.exe
-xcopy /e /h /y %LOCAL_RESOURCE_TMP_DIR%pyodbc\PLATLIB\* %PYTHON_INSTALL_DIR%\Lib\site-packages
+set PYODBC_X64_INSTALLER=pyodbc-3.0.2.win-amd64-py2.7.exe
+powershell -c "(new-object System.Net.WebClient).DownloadFile('http://pyodbc.googlecode.com/files/%PYODBC_X64_INSTALLER%', '%LOCAL_RESOURCE_TMP_DIR%\%PYODBC_X64_INSTALLER%')"
+%SystemDrive%\Zoo\Tools\7za x -o%LOCAL_RESOURCE_TMP_DIR%\pyodbc -y %LOCAL_RESOURCE_TMP_DIR%\%PYODBC_X64_INSTALLER%
+xcopy /e /h /y %LOCAL_RESOURCE_TMP_DIR%\pyodbc\PLATLIB\* %PYTHON_INSTALL_DIR%\Lib\site-packages
 
 :INSTALL_DJANGO_PYODBC
 echo installing django-pyodbc...
 %PYTHON_INSTALL_DIR%\python -c "import sql_server.pyodbc" 2>nul
-if "%ERRORLEVEL%"=="0" goto INSTALL_MERCURIAL
+if "%ERRORLEVEL%"=="0" goto INSTALL_HG
 cd /d %LOCAL_RESOURCE_TMP_DIR%
-%LOCAL_RESOURCE_TMP_DIR%svn-win32\bin\svn export http://django-pyodbc.googlecode.com/svn/trunk/ django-pyodbc
+%LOCAL_RESOURCE_TMP_DIR%\%SVN_DIR%\bin\svn export http://django-pyodbc.googlecode.com/svn/trunk/ django-pyodbc
 cd .\django-pyodbc
 %PYTHON_INSTALL_DIR%\python setup.py install
 cd /d "%~dp0"
 
 
 REM install django-mssql
-:INSTALL_MERCURIAL
+:INSTALL_HG
 echo installing mercurial...
 if exist "%ProgramFiles%\Mercurial\hg.exe" goto INSTALL_PYWIN32
-set MERCURIAL_INSTALLER=mercurial-2.1.1-x86.msi
-if exist "%ProgramFiles(x86)%" set MERCURIAL_INSTALLER=mercurial-2.1.1-x64.msi
-powershell -c "(new-object System.Net.WebClient).DownloadFile('http://mercurial.selenic.com/release/windows/%MERCURIAL_INSTALLER%', '%LOCAL_RESOURCE_TMP_DIR%mercurial.msi')"
-msiexec /i %LOCAL_RESOURCE_TMP_DIR%mercurial.msi /qn
+set HG_INSTALLER=Mercurial-2.1.2-x86.exe
+if exist "%ProgramFiles(x86)%" set HG_INSTALLER=Mercurial-2.1.2-x64.exe
+powershell -c "(new-object System.Net.WebClient).DownloadFile('http://mercurial.selenic.com/release/windows/%HG_INSTALLER%', '%LOCAL_RESOURCE_TMP_DIR%\%HG_INSTALLER%')"
+%LOCAL_RESOURCE_TMP_DIR%\%HG_INSTALLER% /verysilent /sp-
 set PATH=%PATH%;%ProgramFiles%\Mercurial
-if not exist "%ProgramFiles%\Mercurial\hg.exe" echo ERROR: failed to install mercurial.
 
 :INSTALL_PYWIN32
 echo installing pywin32...
@@ -86,10 +89,10 @@ echo installing pywin32...
 if "%ERRORLEVEL%"=="0" goto INSTALL_DJANGO_MSSQL
 set PYWIN32_INSTALLER=pywin32-217.win32-py2.7.exe
 if "%PYTHON_X86%"=="0" set PYWIN32_INSTALLER=pywin32-217.win-amd64-py2.7.exe
-powershell -c "(new-object System.Net.WebClient).DownloadFile('http://downloads.sourceforge.net/project/pywin32/pywin32/Build 217/%PYWIN32_INSTALLER%', '%LOCAL_RESOURCE_TMP_DIR%pywin32.exe')"
-%SystemDrive%\Zoo\Tools\7za x -o%LOCAL_RESOURCE_TMP_DIR%pywin32 -y %LOCAL_RESOURCE_TMP_DIR%pywin32.exe
-xcopy /e /h /y %LOCAL_RESOURCE_TMP_DIR%pywin32\PLATLIB\* %PYTHON_INSTALL_DIR%\Lib\site-packages
-copy %LOCAL_RESOURCE_TMP_DIR%pywin32\SCRIPTS\* %PYTHON_INSTALL_DIR%\Scripts\
+powershell -c "(new-object System.Net.WebClient).DownloadFile('http://downloads.sourceforge.net/project/pywin32/pywin32/Build 217/%PYWIN32_INSTALLER%', '%LOCAL_RESOURCE_TMP_DIR%\%PYWIN32_INSTALLER%')"
+%SystemDrive%\Zoo\Tools\7za x -o%LOCAL_RESOURCE_TMP_DIR%\pywin32 -y %LOCAL_RESOURCE_TMP_DIR%\%PYWIN32_INSTALLER%
+xcopy /e /h /y %LOCAL_RESOURCE_TMP_DIR%\pywin32\PLATLIB\* %PYTHON_INSTALL_DIR%\Lib\site-packages
+copy %LOCAL_RESOURCE_TMP_DIR%\pywin32\SCRIPTS\* %PYTHON_INSTALL_DIR%\Scripts\
 %PYTHON_INSTALL_DIR%\python %PYTHON_INSTALL_DIR%\Scripts\pywin32_postinstall.py -install
 
 :INSTALL_DJANGO_MSSQL
@@ -99,7 +102,7 @@ if "%ERRORLEVEL%"=="0" goto INSTALL_REQUIREMENTS
 %SystemDrive%\Python27\Scripts\pip install hg+https://django-mssql.googlecode.com/hg/#egg=django-mssql
 
 
-REM install other pure python packages
+REM install other required python packages
 :INSTALL_REQUIREMENTS
 echo installing required packages...
 %SystemDrive%\Python27\Scripts\pip install --timeout=600 --default-timeout=600 -r ..\requirements.txt
--- WebRole/bin/install.cmd
+++ WebRole/bin/install.cmd
@@ -2,12 +2,13 @@
 
 echo Starting installation...
 
-if not "%1"=="" set PYTHON_ARCH=.%1
+if "%1"=="amd64" set PYTHON_ARCH=.%1
 
 REM Helicon Zoo requires Python to be there
 set PYTHON_INSTALL_DIR=%SystemDrive%\Python27
 
-if "%LOCAL_RESOURCE_TMP_DIR%"=="" set LOCAL_RESOURCE_TMP_DIR=%TMP%\
+if "%LOCAL_RESOURCE_TMP_DIR%"=="" set LOCAL_RESOURCE_TMP_DIR=%TMP%
+if "%LOCAL_RESOURCE_TMP_DIR:~-1%"=="\" set LOCAL_RESOURCE_TMP_DIR=%LOCAL_RESOURCE_TMP_DIR:~0,-1%
 
 cd /d "%~dp0"
 
@@ -15,15 +16,19 @@ md "%~dp0appdata"
 reg add "hku\.default\software\microsoft\windows\currentversion\explorer\user shell folders" /v "Local AppData" /t REG_EXPAND_SZ /d "%~dp0appdata" /f
 
 echo installing Python...
-powershell -c "(new-object System.Net.WebClient).DownloadFile('http://www.python.org/ftp/python/2.7.2/python-2.7.2%PYTHON_ARCH%.msi', '%LOCAL_RESOURCE_TMP_DIR%python27.msi')"
-msiexec /i %LOCAL_RESOURCE_TMP_DIR%python27.msi /qn TARGET_DIR=%PYTHON_INSTALL_DIR%
+set PYTHON_VERSION=2.7.2
+set PYTHON_INSTALLER=python-%PYTHON_VERSION%%PYTHON_ARCH%.msi
+powershell -c "(new-object System.Net.WebClient).DownloadFile('http://www.python.org/ftp/python/%PYTHON_VERSION%/%PYTHON_INSTALLER%', '%LOCAL_RESOURCE_TMP_DIR%\%PYTHON_INSTALLER%')"
+msiexec /i %LOCAL_RESOURCE_TMP_DIR%\%PYTHON_INSTALLER% /qn TARGET_DIR=%PYTHON_INSTALL_DIR%
 
 echo installing WebPICmdLine...
-powershell -c "(new-object System.Net.WebClient).DownloadFile('http://www.iis.net/community/files/webpi/webpicmdline_anycpu.zip', '%LOCAL_RESOURCE_TMP_DIR%webpicmdline.zip')"
-%PYTHON_INSTALL_DIR%\python -m zipfile -e %LOCAL_RESOURCE_TMP_DIR%webpicmdline.zip %LOCAL_RESOURCE_TMP_DIR%webpicmdline
+set WEBPI_DIR=webpicmdline_anycpu
+set WEBPI_INSTALLER=%WEBPI_DIR%.zip
+powershell -c "(new-object System.Net.WebClient).DownloadFile('http://www.iis.net/community/files/webpi/%WEBPI_INSTALLER%', '%LOCAL_RESOURCE_TMP_DIR%\%WEBPI_INSTALLER%')"
+%PYTHON_INSTALL_DIR%\python -m zipfile -e %LOCAL_RESOURCE_TMP_DIR%\%WEBPI_INSTALLER% %LOCAL_RESOURCE_TMP_DIR%\%WEBPI_DIR%
 
 echo installing Helicon Zoo...
-%LOCAL_RESOURCE_TMP_DIR%webpicmdline\webpicmdline /Products:HeliconZooModule /Feeds:http://www.helicontech.com/zoo/feed/ /AcceptEula
+%LOCAL_RESOURCE_TMP_DIR%\%WEBPI_DIR%\webpicmdline /Products:HeliconZooModule /Feeds:http://www.helicontech.com/zoo/feed/ /AcceptEula
 
 call "%~dp0"install-requirements
 
