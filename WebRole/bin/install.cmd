efc3496796ab89e9eacbd98cfada2cb358459f28
Michiya Takahashi <michiya.takahashi@gmail.com>
2012-04-14 00:35:27 +0900

    hello, python2.7.3!

--- WebRole/bin/install.cmd
+++ WebRole/bin/install.cmd
@@ -2,8 +2,6 @@
 
 echo Starting installation...
 
-if "%1"=="amd64" set PYTHON_ARCH=.%1
-
 REM Helicon Zoo requires Python to be there
 set PYTHON_INSTALL_DIR=%SystemDrive%\Python27
 
@@ -16,8 +14,9 @@ md "%~dp0appdata"
 reg add "hku\.default\software\microsoft\windows\currentversion\explorer\user shell folders" /v "Local AppData" /t REG_EXPAND_SZ /d "%~dp0appdata" /f
 
 echo installing Python...
-set PYTHON_VERSION=2.7.2
-set PYTHON_INSTALLER=python-%PYTHON_VERSION%%PYTHON_ARCH%.msi
+set PYTHON_VERSION=2.7.3
+set PYTHON_INSTALLER=python-%PYTHON_VERSION%.msi
+if "%1"=="amd64" set PYTHON_INSTALLER=python-%PYTHON_VERSION%.%1.msi
 powershell -c "(new-object System.Net.WebClient).DownloadFile('http://www.python.org/ftp/python/%PYTHON_VERSION%/%PYTHON_INSTALLER%', '%LOCAL_RESOURCE_TMP_DIR%\%PYTHON_INSTALLER%')"
 msiexec /i %LOCAL_RESOURCE_TMP_DIR%\%PYTHON_INSTALLER% /qn TARGET_DIR=%PYTHON_INSTALL_DIR%
 
